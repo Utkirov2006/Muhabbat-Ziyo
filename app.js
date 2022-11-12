d94df960ad405120759ad19a20243ef666ed649c
Utkirov Boburbek <boburbekutkirov10@gmail.com>
2022-11-12 17:47:29 +0500

    Ok

---
+++ app.js
@@ -0,0 +1,8 @@
+let swiper = new Swiper(".swiperSlider", {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+      },
+});
